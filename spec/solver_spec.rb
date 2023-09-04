60f1270291959a5b1cad36f6549396b65eead110
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 23:22:33 +0500

    Add test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,19 +2,35 @@ require_relative '../solver'
 
 describe Solver do
   context '#factorial' do
-
-    before(:each) do
-      solver = described_class.new
-    end
     
+    it 'accepts 1 arguments' do
+      expect(Solver.new.method(:factorial).arity).to eq(1)
+    end
+
     it 'Factorial should return correct number' do
       solver = described_class.new
       expect(solver.factorial(5)).to eq 120
     end
 
+    it 'Factorial should return 1 when n is 0' do
+      solver = described_class.new
+      expect(solver.factorial(0)).to eq 1
+    end
+
     it 'Factorial should raise exeption on negative number' do
       solver = described_class.new
       expect {solver.factorial(-5)}.to raise_error('Factorial is undefined for negative numbers')
+    end
+  end
+
+  context '#reverse' do
+    it 'reverse should take one argument' do
+      expect(Solver.new.method(:factorial).arity).to eq(1)
+    end
+
+    it 'reverse should return reversed string' do
+      solver = described_class.new
+      expect(solver.reverse('hello')).to eq 'olleh'
     end
   end
 end
