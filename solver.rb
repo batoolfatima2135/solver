3392c7f040174ff3cf035c6d5564783da74cf1ea
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 23:23:59 +0500

    Modify class to add reverse method

--- solver.rb
+++ solver.rb
@@ -1,9 +1,10 @@
 class Solver
-  def factorial(n)
+  def factorial n
    raise 'Factorial is undefined for negative numbers' if n < 0
    n <= 1 ? 1 : n * factorial(n - 1)
   end
-end
 
-s = Solver.new
-puts s.factorial(5)
+  def reverse word
+   word.reverse
+  end
+end
