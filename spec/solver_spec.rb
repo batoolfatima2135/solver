93482a26143f6cbd0334e58349905793acd1d019
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 22:50:52 +0500

    Add second testcase for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,10 +2,19 @@ require_relative '../solver'
 
 describe Solver do
   context '#factorial' do
+
+    before(:each) do
+      solver = described_class.new
+    end
     
     it 'Factorial should return correct number' do
-    solver = described_class.new
-    expect(solver.factorial(5)).to eq 120
+      solver = described_class.new
+      expect(solver.factorial(5)).to eq 120
+    end
+
+    it 'Factorial should raise exeption on negative number' do
+      solver = described_class.new
+      expect(solver.factorial(-5)).to raise_error(NegativeNumberError)
     end
   end
 end
