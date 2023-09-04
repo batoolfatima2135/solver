e15b7c185b6a25362ed7874593274e8cf8154764
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-04 22:37:59 +0500

    add first test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,9 @@
+describe Solver do
+  context '#factorial' do
+    
+    it 'Factorial should return correct number' do
+    solver = described_class.new
+    expect(solver.factorial(5)).to eq 120
+    end
+  end
+end
