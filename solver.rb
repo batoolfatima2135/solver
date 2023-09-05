ce7f932b50f578d191e06cf27f44d884d3470f42
moFuhidy <mo7ammed9290@gmail.com>
2023-09-05 11:06:35 +0300

    Add third condition for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -10,7 +10,9 @@ class Solver
   end
 
   def fizzbuzz num
-   if num % 3 == 0
+   if num % 3 == 0 && num % 5 == 0
+    'fizzbuzz'
+   elsif num % 3 == 0
     'fizz'
    elsif num % 5 == 0
     'buzz'
